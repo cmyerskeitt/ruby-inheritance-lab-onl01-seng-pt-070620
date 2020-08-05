2a854cdf1753fb087729f137da217fa55ddde37b
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-05 03:49:56 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,9 +1,4 @@
 class User 
   attr_accessor :first_name, :last_name, :knowledge
-  
-  def initialize(first_name,last_name, knowledge)
-    @first_name= first_name
-    @last_name= last_name
-    @knowledge = knowledge
-  end 
+
 end 
